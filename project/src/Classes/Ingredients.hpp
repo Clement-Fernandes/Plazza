beeb20ebfe9c2c7e077b418c9e2f7283865cf877
Clement Fernandes <clement.fernandes@epitech.eu>
2022-04-28 09:17:40 +0200

    [ADD] Interface Mutex

---
+++ project/src/Classes/Ingredients.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2022
+** B-CCP-400-PAR-4-1-theplazza-clement.fernandes
+** File description:
+** Ingredients
+*/
+
+#ifndef INGREDIENTS_HPP_
+#define INGREDIENTS_HPP_
+
+enum class Ingredients {
+    DOE = 1,
+    TOMATO = 2,
+    GRUYERE = 3,
+    HAM = 4,
+    MUSHROOMS = 5,
+    STEAK = 6,
+    EGGPLANT = 7,
+    GOAT_CHEESE = 8,
+    CHIEF_LOVE = 9
+};
+
+#endif /* !INGREDIENTS_HPP_ */
