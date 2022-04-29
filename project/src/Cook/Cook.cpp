6f8be7b3fefac74337bb6a0282b472ace7d5bf2e
Clement Fernandes <clement.fernandes@epitech.eu>
2022-04-29 10:10:47 +0200

    [ADD] Cook class

---
+++ project/src/Cook/Cook.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** B-CCP-400-PAR-4-1-theplazza-clement.fernandes
+** File description:
+** Cook
+*/
+
+#include "Cook.hpp"
+
+Cook::Cook()
+{
+}
+
+Cook::~Cook()
+{
+}
---
+++ project/src/Cook/Cook.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** B-CCP-400-PAR-4-1-theplazza-clement.fernandes
+** File description:
+** Cook
+*/
+
+#ifndef COOK_HPP_
+#define COOK_HPP_
+
+class Cook {
+    public:
+        Cook();
+        ~Cook();
+
+    protected:
+    private:
+};
+
+#endif /* !COOK_HPP_ */
