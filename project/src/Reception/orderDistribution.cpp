692249f7a5a220328c9a393c718708a6044b46d7
Maxime Carabina <maxime.carabina@epitech.eu>
2022-05-03 17:21:53 +0200

    [FIX] problem whith kitchen creation

--- project/src/Reception/orderDistribution.cpp
+++ project/src/Reception/orderDistribution.cpp
@@ -10,7 +10,7 @@
 
 void Reception::addKitchen()
 {
-    std::cout << "create new Kitchen" << std::endl;
+    // std::cout << "Create new Kitchen" << std::endl;
     IPC comReception;
     IPC comKitchen;
 
@@ -33,26 +33,38 @@ void Reception::orderDistribution(std::vector<Order> const &orderList)
 {
     size_t kitchenId;
     std::string response;
+    // int it = 0;
 
-    for (auto i = orderList.begin(); i != orderList.end();) {
+    for (auto i = orderList.begin(); i != orderList.end(); kitchenId++) {
         bool messageGot = false;
-        for (kitchenId = 0; kitchenId < _listKitchen.size() && !messageGot; kitchenId++) {
+        // std::cout << "Iteration : " << it << ", Pizza type : " << i->getType() << std::endl;
+
+        for (kitchenId = 0; kitchenId < _listKitchen.size(); kitchenId++) {
             std::string pizza = std::to_string(i->getType()) + " " + std::to_string(i->getSize());
             bool readed = false;
 
             _listKitchen[kitchenId]["write"] << pizza;
+
             while (!readed) {
                 try {
                     _listKitchen[kitchenId]["read"] >> response;
                     readed = true;
-                    if (response == "yes")
+                    if (response == "y") {
                         messageGot = true;
+                    }
                 } catch (Error::IPC const &) {}
             }
+            if (messageGot == true)
+                break;
         }
+        // std::cout << "kitchenID : " << kitchenId << std::endl;
         if (kitchenId == _listKitchen.size())
             addKitchen();
-        else
+        else {
+            // std::cout << "Pizza " << it << " attribué !" << std::endl;
+            // std::cout << " " << std::endl;
+            // it++;
             i++;
+        }
     }
 }
